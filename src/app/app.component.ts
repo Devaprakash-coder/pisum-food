134e4b4e9ecd9191723ef0858742ded42542be46
Deva <devaprakash.m1997@gmail.com>
2021-06-30 17:15:37 +0530

    responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,37 @@ export class AppComponent {
   title = 'pisumfoods';
   constructor(@Inject(DOCUMENT) private document: Document) { }
 
+
   @HostListener('window:scroll', [])
-  onWindowScroll() {
+  onWindowScrolls() {
     if (document.body.scrollTop > 80 ||     
     document.documentElement.scrollTop > 80) {
       document.getElementById('nav')!.classList.add('nav-scroll');
       document.getElementById('logo')!.classList.add('logo-scroll');
+      document.getElementById('logos')!.classList.add('logo-scroll');
+      document.getElementById('small-nav')!.classList.add('small-nav-scroll');
     }else{
       document.getElementById('nav')!.classList.remove('nav-scroll');
       document.getElementById('logo')!.classList.remove('logo-scroll');
+      document.getElementById('logos')!.classList.remove('logo-scroll');
+      document.getElementById('small-nav')!.classList.remove('small-nav-scroll');
     }
   }
+
+  showList(){
+    document.getElementById('icon1')!.classList.add('hide_icon');
+    document.getElementById('icon1')!.classList.remove('show_icon');
+    document.getElementById('icon2')!.classList.add('show_icon');
+    document.getElementById('icon2')!.classList.remove('hide_icon');
+    document.getElementById('list')!.classList.add('list_show');
+  }
+  hideList(){
+    document.getElementById('list')!.classList.remove('list_show');
+    document.getElementById('list')!.classList.add('list_hide');
+    document.getElementById('icon1')!.classList.add('show_icon');
+    document.getElementById('icon1')!.classList.remove('hide_icon');
+    document.getElementById('icon2')!.classList.remove('show_icon');
+    document.getElementById('icon2')!.classList.add('hide_icon');
+  }
+
 }
